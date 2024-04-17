30f1dc2829d259d3dc9ea9acff05f95120fb3000
happy663 <tatu2425@gmail.com>
2024-04-18 07:50:34 +0900

    update

--- estimate_trajectory.py
+++ estimate_trajectory.py
@@ -1,12 +1,14 @@
-
 import sys
-from dataset.src.sub import estimate
 
-if __name__ == '__main__':
-    print(sys.argv)
-    if len(sys.argv) != 3:
-        print("使用方法: python3 estimate_trajectory_include_ble.py <trial_file_without_extension>" +
-              " <mode: pdr or ble>")
+from src.sub import estimate
+
+if __name__ == "__main__":
+    REQUIRED_ARGUMENTS = 3
+    if len(sys.argv) != REQUIRED_ARGUMENTS:
+        print(
+            "使用方法: python3 estimate_trajectory_include_ble.py <trial_file_without_extension>"
+            " <mode: pdr or ble>",
+        )
         sys.exit(1)
 
     trial_filename_base = sys.argv[1]
@@ -14,11 +16,15 @@ if __name__ == '__main__':
 
     if mode == "pdr":
         estimate.output_estimate_trajectory_pdr(
-            './dataset/trials/', trial_filename_base +
-            '_pdr.txt', './dataset/traj/', trial_filename_base + '_pdr_est'
+            "./dataset/trials/",
+            trial_filename_base + "_pdr.txt",
+            "./dataset/traj/",
+            trial_filename_base + "_pdr_est",
         )
     elif mode == "ble":
         estimate.output_estimate_trajectory_include_ble(
-            './dataset/trials/', trial_filename_base +
-            '.txt', './dataset/traj/', trial_filename_base + '_est'
+            "./dataset/trials/",
+            trial_filename_base + ".txt",
+            "./dataset/traj/",
+            trial_filename_base + "_est",
         )
